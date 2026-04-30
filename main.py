80930b313959de3b70a05293e0d0ffe02c6adb15
Xaash-dusty <savely88l@gmail.com>
2026-04-30 10:42:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ def handle_all_messages(message):
             except Exception as e:
                 bot.send_message(message.chat.id, f"Ошибка: {e}")
         else:
-            if message.text != "я":
+            if text != "я":
             # Если никакого режима нет и это не команда — тогда уже пишем "Не понимаю" или просто текст
                 bot.send_message(message.chat.id, "❓ Я тебя не понимаю. Используй кнопки меню.")
             
